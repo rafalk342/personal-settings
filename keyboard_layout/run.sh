66841290c34deb46c4f16354ec7e068ab70d7177
rafalk342 <rafalk342@gmail.com>
2024-03-12 20:51:49 +0100

    Remove numbers from base layer to speed up learning.

--- keyboard_layout/run.sh
+++ keyboard_layout/run.sh
@@ -1,4 +1,5 @@
 {
 	sleep 3
-	xkbcomp -w10 ~/code/personal-settings/keyboard_layout/keyboard_layout.xkb_keymap $DISPLAY
+	#xkbcomp -w10 ~/code/personal-settings/keyboard_layout/keyboard_layout.xkb_keymap $DISPLAY
+	kmonad ~/code/personal-settings/keyboard_layout/kmonad_layout.kbd
 } &
